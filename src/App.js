6745b351858ee90b8cdf1243dcac86e0b950854d
AleyCabrera <cabreratrader.fxoficry@gmail.com>
2024-11-13 12:00:48 -0500

    Agregacion de archivos nuevos y modificacion de archivos existentes en el proyecto

--- src/App.js
+++ src/App.js
@@ -27,14 +27,4 @@ function App() {
   );
 }
 
-function TodoItem() {
-  return (
-    <li>
-      <span>V</span>
-      <p>LLorar con la llorona</p>
-      <p>X</p>
-    </li>
-  );
-}
-
 export default App;
